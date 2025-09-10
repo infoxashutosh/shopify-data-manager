1bed49f252e370fc952c7273c4937a36cd76dc2f
infoxashutosh <infoxashutosh@gmail.com>
2025-09-10 11:16:08 -0400

    Remove API key display from homepage route for security

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Shopify Data Manager server is running!');
-});
+    res.send('Shopify Data Manager server is running!<br>Studio Economik API Key: ' + process.env.STUDIO_API_KEY);
+  });
+
 
 app.listen(PORT, () => {
   console.log(`Server is listening on http://localhost:${PORT}`);
